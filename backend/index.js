fd74b8b2277c268bcfd4dbf8b2f293db8292d65c
simsam897 <simma013s@gmail.com>
2026-08-06 12:02:54 +0530

    add install cors and added cors to index.js file

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,14 @@ import authRouter from "./src/routes/auth.route.js";
 dotenv.config({ path: ".env" });
 import express from "express";
 import mongoose from "mongoose";
-
+import cors from "cors";
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use("/auth", authRouter);
 
