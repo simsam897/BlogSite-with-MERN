645a828e306e8159c01cc20e570b4bf46585529c
simsam897 <simma013s@gmail.com>
2026-08-07 13:20:26 +0530

    add dark mode theme functionality

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 const MainLayout = () => {
+
+  const mode = useSelector((state) => state.theme.mode)
+
+  useEffect(() => {
+    document.documentElement.classList.toggle(
+      "dark",
+      mode === "dark"
+    )
+  }, [mode])
   return (
     <>
       <Navbar />
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice.js";
-
+import themeReducer from "../features/theme/themeSlice.jsx";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-
+    theme: themeReducer,
   },
 });
